cb54aaa47a43aea14fdc17f34f430b01b5864d57
131MohitYadav <yadavjimohit54@gmail.com>
2025-01-27 19:14:39 +0530

    27 Jan 2025

--- JAVASCRIPT LANGUAGE/obj.js
+++ JAVASCRIPT LANGUAGE/obj.js
@@ -18,12 +18,16 @@ function fun(){
 let ob1 = {
     
     nam : document.querySelector('#name').value,
-    age : document.querySelector('#age').value
+    age : document.querySelector('#age').value,
+    email : document.querySelector('#email').value,
+    pass : document.querySelector('#pass').value,
+    cont : document.querySelector('#cont').value,
+    
     
    
 }
 
-console.log(ob1.nam , "\n" , ob1.age);
+console.log(ob1.nam , "\n" , ob1.age , "\n", ob1.email , "\n" , ob1.pass , "\n" , ob1.cont);
 return false
 
 
