284b7be918e5c6667bd19e04f7f16b825ff5e9df
131MohitYadav <yadavjimohit54@gmail.com>
2025-01-30 08:40:11 +0530

    30 Jan 2025

--- JAVASCRIPT LANGUAGE/Task_js.js
+++ JAVASCRIPT LANGUAGE/Task_js.js
@@ -5,3 +5,46 @@ function color(){
     document.body.style.backgroundColor=clr;
     return false
 }
+
+
+function fun3(){
+    let image1 = document.getElementById('image1')
+    image1.src = "gul.jpeg"
+    
+}
+
+
+function fun2(){
+    let image1 = document.getElementById('image1')
+    image1.src = "ras.jpeg"
+    
+}
+
+function fun1(){
+    let image1 = document.getElementById('image1')
+    image1.src = "malai.jpeg"
+    
+}
+let count = 0;
+let head = document.getElementById('head')
+
+function inc(){
+    count = count + 1;
+    head.innerHTML = count;
+    
+    
+}
+
+function dec(){
+    count = count - 1;
+    head.innerHTML = count;
+    
+    
+}
+
+function res(){
+    count = 0;
+    head.innerHTML = count;
+    
+    
+}
