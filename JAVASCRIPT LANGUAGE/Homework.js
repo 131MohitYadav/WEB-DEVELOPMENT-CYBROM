00e45f14c2d66ea7797aba89b0035fa5af3668e4
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-05 15:57:03 +0530

    5 feb 2025

--- JAVASCRIPT LANGUAGE/Homework.js
+++ JAVASCRIPT LANGUAGE/Homework.js
@@ -1,17 +1,28 @@
 
-function display(){
+function fun(){
+    
+    alert("form submit successfully")
+    return true;
+
+}
+   // let display = document.getElementById('display')
+
+   function display(){
     let ob1 = {
         
-        nam : document.querySelector('#name').value,
-        age : document.querySelector('#age').value,
-        email : document.querySelector('#email').value,
-        pass : document.querySelector('#pass').value,
-        cont : document.querySelector('#cont').value,
+        nam : document.getElementById('name').value,
+        age : document.getElementById('age').value,
+        email : document.getElementById('email').value,
+        pass : document.getElementById('pass').value,
+        cont : document.getElementById('cont').value
         
         
        
-    }
+    };
 
-    console.log(ob1.nam , "\n" , ob1.age , "\n", ob1.email , "\n" , ob1.pass , "\n" , ob1.cont);
-    return false
-    }    
+    // console.log(ob1.nam , "\n" , ob1.age , "\n", ob1.email , "\n" , ob1.pass , "\n" , ob1.cont);
+    console.log(ob1);
+    
+    
+    
+   }
