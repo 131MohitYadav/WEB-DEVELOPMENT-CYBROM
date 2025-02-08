013138e700656f3c8aa1464b55d554d78ec0f772
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-08 18:58:48 +0530

    8 feb 2025

--- JAVASCRIPT LANGUAGE/8_task.js
+++ JAVASCRIPT LANGUAGE/8_task.js
@@ -1,21 +1,22 @@
 // let frm = document.getElementById('myform')
 // frm.body.style.backgroundColor = "red";
-   function fun(){
-    let name = document.getElementById("name").value;
-    let cont = document.getElementById("cont").value;
-    let eml = document.getElementById("email").value;
-    let cty = document.getElementById("city").value;
-    
-    localStorage.setItem("username",name);
-    localStorage.setItem("contact",cont);
+function fun() {
+   let name = document.getElementById("name").value;
+   let cont = document.getElementById("cont").value;
+   let eml = document.getElementById("email").value;
+   let cty = document.getElementById("city").value;
 
-    localStorage.setItem("email",eml);
-    localStorage.setItem("city",cty);
-    
-    window.location.href="Submit.html";
-return false;
-    
-    
+   
 
-   }
-    
+   localStorage.setItem("username", name);
+   localStorage.setItem("contact", cont);
+
+   localStorage.setItem("email", eml);
+   localStorage.setItem("city", cty);
+
+   window.location.href = "Submit.html";
+   return false;
+
+
+
+}
