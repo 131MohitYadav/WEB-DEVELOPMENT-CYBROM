ea5bf970c5224238c89428024c5933ed46c2e30e
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-12 20:59:23 +0530

    12 feb 2025

--- JAVASCRIPT LANGUAGE/fetch.js
+++ JAVASCRIPT LANGUAGE/fetch.js
@@ -1,10 +1,11 @@
 async function Demo(){
 
-    // fetch () -> it used for get the data from the server on api
+// fetch () -> it used for get the data from the server on api
 // async -> it allowing it to run without blocking other code.
 // it always return promise 
 // await ->  javascript wait for promise to resolve before moving on
-    let apidata = await fetch("https://jsonplaceholder.typicode.com/comments")
+ let apidata = await
+ fetch("https://jsonplaceholder.typicode.com/comments")
     let fdata = await apidata.json() // convert to json object
     let api = document.querySelector('#apidata')
     let dt = fdata.map((e) => {
