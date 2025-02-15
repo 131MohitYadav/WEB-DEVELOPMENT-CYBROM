dc9a97068d958ad33e1d583c89750f395560131a
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-15 17:06:00 +0530

    15 Feb 2025

--- JAVASCRIPT LANGUAGE/data.js
+++ JAVASCRIPT LANGUAGE/data.js
@@ -22,3 +22,20 @@ function mydel(id){
     .then(r=>alert("Delete successfuly....!!!!"))
 
 }
+
+function insertdata(){
+    let frmdata = {
+        name:document.querySelector('#name').value,
+        age:document.querySelector('#age').value,
+        contact:document.querySelector('#contact').value,
+        city:document.querySelector('#city').value,
+    }
+    fetch('http://localhost:3000/student',{
+        method:'POST',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(frmdata)
+    })
+    .then(r=>alert("data inserted....!!!!"))
+}
