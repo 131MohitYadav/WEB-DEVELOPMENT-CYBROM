48ce90f460e13d567732965115f9de3d0e68277a
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-23 20:32:30 +0530

    23 Feb 2025

--- React Js/secondproject/src/App.jsx
+++ React Js/secondproject/src/App.jsx
@@ -7,12 +7,13 @@ function App() {
   <h1>Welcome to React Class</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ab saepe dolor optio amet aspernatur, quidem deleniti sint animi possimus? Adipisci distinctio quasi ipsum ipsam dolorem dignissimos vitae temporibus beatae.</p>
 
-  <h2>This is a Image</h2>
+  <h2 > This is a Ordered List</h2>
 
-  <ul type = "a">
+  <ol type = "1">
     <li>Mohit</li>
     <li>Rohit</li>
-  </ul>
+  </ol>
+  <h2>This is a Image</h2>
   <img src="image.jpg"></img>
  </div>
 
