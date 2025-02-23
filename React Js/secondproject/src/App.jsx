fa4d13ce2f6a25d56cb169b4986cd24e0fb437e0
131MohitYadav <yadavjimohit54@gmail.com>
2025-02-23 20:28:06 +0530

    23 Feb 2025

--- React Js/secondproject/src/App.jsx
+++ React Js/secondproject/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ab saepe dolor optio amet aspernatur, quidem deleniti sint animi possimus? Adipisci distinctio quasi ipsum ipsam dolorem dignissimos vitae temporibus beatae.</p>
 
   <h2>This is a Image</h2>
+
+  <ul type = "a">
+    <li>Mohit</li>
+    <li>Rohit</li>
+  </ul>
   <img src="image.jpg"></img>
  </div>
 
