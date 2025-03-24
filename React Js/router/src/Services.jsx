18ee6b7bc64c15f446c87a9fc6c37a94980d91ce
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-24 21:58:55 +0530

    24 march 2025

--- React Js/router/src/Services.jsx
+++ React Js/router/src/Services.jsx
@@ -2,6 +2,9 @@ function Services(){
     return(
         <>
         <h1>Services Page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit ut ea veritatis voluptatum ipsam, provident beatae voluptas, reprehenderit quam distinctio modi cum iusto voluptate. Delectus ex quibusdam cum corporis inventore!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum iusto laboriosam eos ipsa obcaecati adipisci aliquid, quis eveniet ullam omnis laborum saepe tenetur aperiam rerum repellat porro, quisquam, accusantium architecto?
+        </p>
         </>
     )
 }
