3613efa5116f2fcdb7519ff012e9217c18e579ae
131MohitYadav <yadavjimohit54@gmail.com>
2025-03-27 19:06:57 +0530

    27 march 2025

--- React Js/form/src/App.jsx
+++ React Js/form/src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
-//import Form from './Data';
+// import Data from './Data';
 
 
 function App() {
--- React Js/form/src/Data.jsx
+++ React Js/form/src/Data.jsx
@@ -1,59 +1,59 @@
-import React, { useState } from 'react';
-import Data from './Data';
+// import React, { useState } from 'react';
+// import Data from './Data';
 
 
-function Data() {
+// function Data() {
 
-let[data,setData]=useState({
-    us:"" , age1:"",email:"",city1:"",add:"",
-});
+// let[data,setData]=useState({
+//     us:"" , age1:"",email:"",city1:"",add:"",
+// });
 
-function senddata(e){
-    const [name,value] = e.target
-    setData({...data,[name]:value});
-}
+// function senddata(e){
+//     const [name,value] = e.target
+//     setData({...data,[name]:value});
+// }
 
-function datacatch(e){
-    e.preventdefault()
-    console.log(senddata)
+// function datacatch(e){
+//     e.preventdefault()
+//     console.log(senddata)
 
-}
+// }
 
 
-  return (
-    <>
+//   return (
+//     <>
 
-    <h1>USER FORM</h1>
-    <form action="" onSubmit={datacatch}>
+//     <h1>USER FORM</h1>
+//     <form action="" onSubmit={datacatch}>
 
-<label htmlFor="">Name: </label>
-<input type='text' name='us' onChange={senddata}></input><br></br>
-<br></br>
-<label htmlFor="">Age: </label>
-<input type='text' name='age1' onChange={senddata}></input>
-<br></br>
-<br></br>
+// <label htmlFor="">Name: </label>
+// <input type='text' name='us' onChange={senddata}/><br></br>
+// <br></br>
+// <label htmlFor="">Age: </label>
+// <input type='text' name='age1' onChange={senddata}/>
+// <br></br>
+// <br></br>
 
-<label htmlFor="">Email: </label>
-<input type='text' name='email' onChange={senddata}></input>
-<br></br>
-<br></br>
+// <label htmlFor="">Email: </label>
+// <input type='text' name='email' onChange={senddata}/>
+// <br></br>
+// <br></br>
 
-<label htmlFor="">City: </label>
-< type='text' name='city1' onChange={senddata}/>
-<br></br>
-<br></br>
-<label htmlFor="">Address: </label>
-< type='text' name='add' onChange={senddata}/>
-<br></br>
-<br></br>
+// <label htmlFor="">City: </label>
+// <input type='text' name='city1' onChange={senddata} />
+// <br></br>
+// <br></br>
+// <label htmlFor="">Address: </label>
+// <input type='text' name='add' onChange={senddata}/>
+// <br></br>
+// <br></br>
 
-<input type='submit' value="Submit" />
+// <input type='submit' value="Submit" />
 
-    </form>
+//     </form>
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-export default Data
+// export default Data
