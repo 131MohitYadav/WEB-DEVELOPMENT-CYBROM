3f4aa4b8e8b692136d2494d454c9996f38d2479e
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-11 09:34:57 +0530

    11 april 2025

--- React Js/auth/src/Login.jsx
+++ React Js/auth/src/Login.jsx
@@ -1,48 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+  const [frmdata , setFrmdata] = useState({
+    name: "", password: "",
+  });
 
-    let [frmdata , setFrmdata] = useState({
-        name:"", password:"",
-      });
-      
-      let navigator = useNavigate()
-      
-      function Inputhandle(f){
-        const{name,value} = f.target 
-        setFrmdata({...frmdata , [name] : value})
-      }
-       
-      function finalSubmit(e){
-        e.preventDefault()
-        console.log(frmdata);
-        localStorage.setItem('logindata', JSON.stringify(frmdata))
-       navigator('/home')
-        
-      
-      }
-      
+  const navigator = useNavigate();
 
+  function Inputhandle(e) {
+    const { name, value } = e.target;
+    setFrmdata({ ...frmdata, [name]: value });
+  }
+
+  function finalSubmit(e) {
+    e.preventDefault();
+
+    const storedData = JSON.parse(localStorage.getItem('signdata'));
+
+    if (
+      storedData &&
+      frmdata.name === storedData.name &&
+      frmdata.password === storedData.password
+    ) {
+      alert("Login Successful");
+      navigator('/home');
+    } else {
+      alert("Invalid Credentials");
+    }
+  }
 
   return (
-   <>
-   
-   <h1>Login Form</h1>
-   <form action="" onSubmit={finalSubmit}>
+    <>
+      <h1>Login Form</h1>
+      <form onSubmit={finalSubmit}>
+        <label>Name: </label>
+        <input type="text" name="name" onChange={Inputhandle} /> <br /><br />
 
-   <label>Name: </label>
-    <input type="text" name="name" onChange={Inputhandle} /> <br></br><br></br>
+        <label>Password: </label>
+        <input type="password" name="password" onChange={Inputhandle} /> <br /><br />
 
-    <label>Password: </label>
-    <input type="text" name="password" onChange={Inputhandle} /> <br></br><br></br>
+        <input type="submit" value="Login" />
+      </form>
+    </>
+  );
+};
 
-    <input type="submit"/>
-    {/* <button onClick={signalert}>SUBMIT</button> */}
-
-   </form>
-   </>
-  )
-}
-
-export default Login
+export default Login;
