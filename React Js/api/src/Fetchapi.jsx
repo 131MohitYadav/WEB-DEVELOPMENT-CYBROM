99017ed5c7b9b1580cf11ee574f73775c7f4bc19
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-12 19:01:53 +0530

    12 april 2025

--- React Js/api/src/Fetchapi.jsx
+++ React Js/api/src/Fetchapi.jsx
@@ -1,16 +1,32 @@
 
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Fetchapi = () => {
 
+    let[apidata,setapidata] = useState()
+
 useEffect(()=> {
     axios.get('http://localhost:3000/userdata')
-    .then(res=>console.log(res.data))
+    .then(res=>setapidata(res.data))
 }, [])
 
   return (
+    <>
   <h1>api data fetch here</h1>
+  <table>
+    <tr>
+        <td>ID</td>
+        <td>U_NAME</td>
+        <td>AGE</td>
+        <td>CONTACT</td>
+        <td>CITY</td>
+    </tr>
+  </table>
+  
+
+
+</>
   )
 }
 
