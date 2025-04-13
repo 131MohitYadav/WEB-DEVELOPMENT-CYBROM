8c783d14d1f318a309b8488a116c388cb457dc53
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-13 10:54:17 +0530

    13 april 2025

--- React Js/api/src/Fetchapi.jsx
+++ React Js/api/src/Fetchapi.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 
 const Fetchapi = () => {
 
-    let[apidata,setapidata] = useState()
+    let[apidata,setapidata] = useState([])
 
 useEffect(()=> {
     axios.get('http://localhost:3000/userdata')
@@ -14,7 +14,8 @@ useEffect(()=> {
   return (
     <>
   <h1>api data fetch here</h1>
-  <table>
+  <h2>ALL USER DATA</h2>
+  <table border="2px solid black" align='center'>
     <tr>
         <td>ID</td>
         <td>U_NAME</td>
@@ -22,6 +23,16 @@ useEffect(()=> {
         <td>CONTACT</td>
         <td>CITY</td>
     </tr>
+    {
+      apidata.map((e)=>{ return <tr>
+        <td>{e.id}</td>
+        <td>{e.uname}</td>
+        <td>{e.age}</td>
+        <td>{e.contact}</td>
+        <td>{e.city}</td>
+
+      </tr>})
+    }
   </table>
   
 
