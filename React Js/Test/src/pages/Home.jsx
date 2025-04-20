7f4f883404a091e32c8113244940c199792070f4
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-20 19:27:50 +0530

    20 april 2025

--- React Js/Test/src/pages/Home.jsx
+++ React Js/Test/src/pages/Home.jsx
@@ -114,6 +114,85 @@ const Home = () => {
 </section>
 
 
+{/* class section */}
+
+<section className="classes-section spad">
+    <div className="container">
+        <div className="row">
+            <div className="col-lg-12">
+                <div className="section-title">
+                    <span>Our Classes</span>
+                    <h2>WHAT WE CAN OFFER</h2>
+                </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-4 col-md-6">
+                <div className="class-item">
+                    <div className="ci-pic">
+                        <img src="/img/class-1.jpg" alt="Weightlifting" />
+                    </div>
+                    <div className="ci-text">
+                        <span>STRENGTH</span>
+                        <h5>Weightlifting</h5>
+                        {/* <a href="#"><i className="fa fa-angle-right"></i></a> */}
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+                <div className="class-item">
+                    <div className="ci-pic">
+                        <img src="/img/class-2.jpg" alt="Indoor Cycling" />
+                    </div>
+                    <div className="ci-text">
+                        <span>Cardio</span>
+                        <h5>Indoor cycling</h5>
+                        {/* <a href="#"><i className="fa fa-angle-right"></i></a> */}
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+                <div className="class-item">
+                    <div className="ci-pic">
+                        <img src="img/class-3.jpg" alt="Kettlebell Power" />
+                    </div>
+                    <div className="ci-text">
+                        <span>STRENGTH</span>
+                        <h5>Kettlebell power</h5>
+                        {/* <a href="#"><i className="fa fa-angle-right"></i></a> */}
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-6 col-md-6">
+                <div className="class-item">
+                    <div className="ci-pic">
+                        <img src="img/class-4.jpg" alt="Indoor Cycling" />
+                    </div>
+                    <div className="ci-text">
+                        <span>Cardio</span>
+                        <h4>Indoor cycling</h4>
+                        {/* <a href="#"><i className="fa fa-angle-right"></i></a> */}
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-6">
+                <div className="class-item">
+                    <div className="ci-pic">
+                        <img src="img/class-5.jpg" alt="Boxing" />
+                    </div>
+                    <div className="ci-text">
+                        <span>Training</span>
+                        <h4>Boxing</h4>
+                        {/* <a href="#"><i className="fa fa-angle-right"></i></a> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
 
 
 
