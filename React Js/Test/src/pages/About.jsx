f03003b5bbf6c16ade240e455fadd17aa455ca0e
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-27 10:29:40 +0530

    27 april  2025

--- React Js/Test/src/pages/About.jsx
+++ React Js/Test/src/pages/About.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
    <>
-
-<section className="breadcrumb-section set-bg" data-setbg="img/breadcrumb-bg.jpg">
-  <div className="container">
-    <div className="row">
-      <div className="col-lg-12 text-center">
-        <div className="breadcrumb-text">
-          <h2>About us</h2>
-          <div className="bt-option">
-            <a href="./Home.jsx">Home</a>
-            <span>About</span>
+<section 
+        className="breadcrumb-section set-bg" 
+        style={{ backgroundImage: `url('img/breadcrumb-bg.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+      >
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <div className="breadcrumb-text">
+                <h2>About us</h2>
+                <div className="bt-option">
+                  <Link to="/">Home</Link>
+                  <span>About</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
    
    </>
   )
