f35f1387c6819718b600d2eb13168c5d31b491c1
131MohitYadav <yadavjimohit54@gmail.com>
2025-04-27 10:24:59 +0530

    27 april 2025

--- React Js/Test/src/pages/About.jsx
+++ React Js/Test/src/pages/About.jsx
@@ -4,7 +4,21 @@ const About = () => {
   return (
    <>
 
-   
+<section className="breadcrumb-section set-bg" data-setbg="img/breadcrumb-bg.jpg">
+  <div className="container">
+    <div className="row">
+      <div className="col-lg-12 text-center">
+        <div className="breadcrumb-text">
+          <h2>About us</h2>
+          <div className="bt-option">
+            <a href="./Home.jsx">Home</a>
+            <span>About</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
    
    </>
   )
