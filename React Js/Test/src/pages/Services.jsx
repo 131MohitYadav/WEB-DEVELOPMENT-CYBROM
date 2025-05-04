be2a6d011c44d641f0d20ddaba5289dc49d43a6d
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-04 17:45:08 +0530

    4 may 2025

--- React Js/Test/src/pages/Services.jsx
+++ React Js/Test/src/pages/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaEnvelope, FaHome , FaPhoneAlt } from 'react-icons/fa';
 
 const Services = () => {
   return (
