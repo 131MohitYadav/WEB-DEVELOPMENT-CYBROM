4fb78c0cb81df520397148ea37bbcfb8c2bef12c
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-15 20:17:10 +0530

    15 may 2025

--- React Js/Test/src/pages/Home.jsx
+++ React Js/Test/src/pages/Home.jsx
@@ -8,10 +8,10 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaEnvelope, FaHome , Fa
 const teamMembers = [
     { name: "Athart Rachel", role: "Gym Trainer", pic: "/img/team-1.jpg" },
     { name: "Gunnar Petterson", role: "Fiteness Trainer", pic: "/img/team-2.jpg" },
-    { name: "Athart Rachel", role: "Gym Trainer", pic: "/img/team-3.jpg" },
+    { name: "", role: "Gym Trainer", pic: "/img/team-3.jpg" },
     { name: "Athart Rachel", role: "Gym Trainer", pic: "/img/team-4.jpg" },
-    { name: "Athart Rachel", role: "Gym Trainer", pic: "/img/team-5.jpg" },
-    { name: "Athart Rachel", role: "Gym Trainer", pic: "/img/team-6.jpg" },
+    { name: "Kiry Myers", role: "Weight Loss Specialist", pic: "/img/team-5.jpg" },
+    { name: "Kathey Kaehler", role: "Fitnees Group Instructor", pic: "/img/team-6.jpg" },
   ];
 
 
