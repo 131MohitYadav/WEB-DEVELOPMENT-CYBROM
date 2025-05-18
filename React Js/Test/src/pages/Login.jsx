06987643136ca5ae4f6c14f599dd85e6757874f5
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-18 11:18:05 +0530

    18 may 2025

---
+++ React Js/Test/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+   <>
+
+   <div className="login21-container">
+    <div className="login21-card">
+        <div className="login21-form-section">
+
+            <h1 className="login21-logo">GY<span>M</span></h1>
+        </div>
+    </div>
+   </div>
+   
+   
+   </>
+  )
+}
+
+export default Login
