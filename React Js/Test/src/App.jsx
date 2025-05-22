221dc7e45c2f5323dd1c6e7b074f4b019901617d
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-22 20:03:05 +0530

    22 may 2025

--- React Js/Test/src/App.jsx
+++ React Js/Test/src/App.jsx
@@ -1,6 +1,5 @@
-import {  Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-// import './Flaticon.css'
 import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,26 +10,24 @@ import Contact from './pages/Contact';
 import Proudct from './Proudct';
 import Login from './pages/Login';
 
-
-
-
 function App() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/' || location.pathname === '/login';
+
   return (
     <>
+      {!isLoginPage && <Header />}
 
-    <Login />
-      <Header />
       <Routes>
-        
-        
-        <Route path="/" element={<Home />} />
+        <Route path="" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/classes" element={<Classes />} />
         <Route path="/services" element={<Services />} />
         <Route path="/team" element={<Team />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/appointment" element={<Proudct />} />
-
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
