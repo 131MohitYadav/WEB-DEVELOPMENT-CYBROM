f66127d7e0d2914bb2778784d3809c6b0e8f096d
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-25 20:05:09 +0530

    25 may 2025

--- React Js/Test/src/pages/Login.jsx
+++ React Js/Test/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         <div className="login21-card">
           <div className="login21-form-section">
             <h1 className="login21-logo">GY<span>M</span></h1>
-            <p className="login21-subtitle">Gym management</p>
+            
             <h2 className="login21-title">Log In</h2>
 
             <input type="email" placeholder="Email" className="login21-input" />
