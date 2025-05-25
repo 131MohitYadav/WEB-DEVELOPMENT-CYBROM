314a8dbff38098d6a1d076b2e53b222dec3dd02b
131MohitYadav <yadavjimohit54@gmail.com>
2025-05-25 20:03:29 +0530

    25 may 2025

--- React Js/Test/src/pages/Register.jsx
+++ React Js/Test/src/pages/Register.jsx
@@ -15,7 +15,7 @@ const Register = () => {
         <div className="login21-card">
           <div className="login21-form-section">
             <h1 className="login21-logo">GY<span>M</span></h1>
-            <p className="login21-subtitle">Gym management</p>
+            
             <h2 className="login21-title">Create Your Account</h2>
 
             <input type="text" placeholder="Username" className="login21-input" />
