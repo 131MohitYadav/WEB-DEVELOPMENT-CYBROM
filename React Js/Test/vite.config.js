652f093809f95719c631e147b7104c14367b7135
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-07 11:49:32 +0530

    7 oct 25

--- React Js/Test/vite.config.js
+++ React Js/Test/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ ensures images, CSS, and JS load correctly after deployment
 })
