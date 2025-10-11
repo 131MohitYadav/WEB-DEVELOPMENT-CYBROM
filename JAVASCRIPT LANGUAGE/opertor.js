1cacf74c75495f67ee2652d51fa17c1e8221c579
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-11 20:14:36 +0530

    11 oct 25

--- JAVASCRIPT LANGUAGE/opertor.js
+++ JAVASCRIPT LANGUAGE/opertor.js
@@ -1,7 +1,25 @@
 
 // write a progrma to print area of circle
-let r = parseFloat(prompt("Enter a  radius of circle"))
-document.write("Area of circle is: " + 3.14*r*r)
+// let r = parseFloat(prompt("Enter a  radius of circle"))
+// document.writeln("Area of circle is: " + 3.14*r*r)
+
+// write to square number 
+
+let n = parseInt(prompt("Enter any number: "))
+document.writeln(n * n);
+
+// to swap two number
+
+let num1 = parseInt(prompt("Enter first number"))
+let num2 = parseInt(prompt("Enter second number"))
+
+
+let num3 = num2;
+num2 = num1;
+
+document.writeln(num3);
+document.writeln(num2);
+
 
 
 2
