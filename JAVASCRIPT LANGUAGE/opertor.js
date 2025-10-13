fb7b80343669e83ea159a0690174362d012fd7b1
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-13 22:46:46 +0530

    13 oct 25

--- JAVASCRIPT LANGUAGE/opertor.js
+++ JAVASCRIPT LANGUAGE/opertor.js
@@ -1,32 +1,34 @@
 
-// write a progrma to print area of circle
-// let r = parseFloat(prompt("Enter a  radius of circle"))
-// document.writeln("Area of circle is: " + 3.14*r*r)
+// // write a progrma to print area of circle
+// // let r = parseFloat(prompt("Enter a  radius of circle"))
+// // document.writeln("Area of circle is: " + 3.14*r*r)
 
-// write to square number 
+// // write to square number 
 
-let n = parseInt(prompt("Enter any number: "))
-document.writeln(n * n);
+// let n = parseInt(prompt("Enter any number: "))
+// document.writeln(n * n);
 
-// to swap two number
+// // to swap two number
 
-let num1 = parseInt(prompt("Enter first number"))
-let num2 = parseInt(prompt("Enter second number"))
-
-
-let num3 = num2;
-num2 = num1;
-
-document.writeln(num3);
-document.writeln(num2);
-
-// write a prorgram to find area of rectangle
-
-let  length = parseInt(prompt("Enter the length:  "));
-let breadth  = parseInt(prompt("Enter the breadth: "));
-
-document.writeln( " ARea of REctangle: " +  length * breadth)
+// let num1 = parseInt(prompt("Enter first number"))
+// let num2 = parseInt(prompt("Enter second number"))
 
 
+// let num3 = num2;
+// num2 = num1;
 
-2
+// document.writeln(num3);
+// document.writeln(num2);
+
+// // write a prorgram to find area of rectangle
+
+// let  length = parseInt(prompt("Enter the length:  "));
+// let breadth  = parseInt(prompt("Enter the breadth: "));
+
+// document.writeln( " ARea of REctangle: " +  length * breadth)
+
+
+
+// FOR IN LOOP REVISTION
+
+let arr = [ 2, 3 ,4 , 52, 23, 35, 233 , 28];
