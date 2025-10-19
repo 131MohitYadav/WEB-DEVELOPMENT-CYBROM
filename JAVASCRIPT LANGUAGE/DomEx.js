1e2ee6248b5851a17a47fbedcd4806bb992d016f
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-19 12:29:41 +0530

    19 oct 25

--- JAVASCRIPT LANGUAGE/DomEx.js
+++ JAVASCRIPT LANGUAGE/DomEx.js
@@ -1,3 +1,9 @@
 // GET ELEMENT BY ID
 const container = document.getElementById('container');
 const myButton = document.getElementById('myButton');
+
+// Get elements by class name (return HTMLCollection)
+const textElements = document.getElementsByClassName('text');
+
+// Get elements by tag name ( return HTMLCollections)
+const paragraphs = document.getElementsByTagName('p');
