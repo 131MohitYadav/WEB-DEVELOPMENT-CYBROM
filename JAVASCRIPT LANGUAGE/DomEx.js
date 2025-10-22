cb8410efda7eb812a3485c7532fe3df89e749881
131MohitYadav <yadavjimohit54@gmail.com>
2025-10-22 08:34:11 +0530

    22 oct 25

--- JAVASCRIPT LANGUAGE/DomEx.js
+++ JAVASCRIPT LANGUAGE/DomEx.js
@@ -7,3 +7,10 @@ const textElements = document.getElementsByClassName('text');
 
 // Get elements by tag name ( return HTMLCollections)
 const paragraphs = document.getElementsByTagName('p');
+
+
+// textContent
+const box = document.getElementById('box')
+
+let b = box.textContent("hello <Strong>mohit</Strong>");
+document.writeln(b);
