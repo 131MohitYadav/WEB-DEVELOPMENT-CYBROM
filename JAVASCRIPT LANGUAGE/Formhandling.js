9356d17be2acffdaab7f5097240fdb7050d2e63b
131MohitYadav <yadavjimohit54@gmail.com>
2025-11-13 20:22:20 +0530

    13 nov 25

---
+++ JAVASCRIPT LANGUAGE/Formhandling.js
@@ -0,0 +1,6 @@
+const form = document.createElement('form');
+form.innerHTML = `
+<input type = "text" id = "name" placeholder = "Enter your name" required>
+<input type = "email" id = "email" placeholder = "Enter your email" required>
+<button type = "submit">Submit</button>
+`;
