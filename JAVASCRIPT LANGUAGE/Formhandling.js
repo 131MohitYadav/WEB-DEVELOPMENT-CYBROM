08227d193f58207d2399f6f31fefc3f8bb2efb75
131MohitYadav <yadavjimohit54@gmail.com>
2025-11-13 21:29:53 +0530

    13 nov 25

--- JAVASCRIPT LANGUAGE/Formhandling.js
+++ JAVASCRIPT LANGUAGE/Formhandling.js
@@ -4,3 +4,25 @@ form.innerHTML = `
 <input type = "email" id = "email" placeholder = "Enter your email" required>
 <button type = "submit">Submit</button>
 `;
+document.body.appendChild(form);
+
+
+form.addEventListener('submit' , (event) => {
+    event.preventDefault(); // prevenet form submission
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
+    alert('Form submitted', {name , email});
+
+    //validation
+    if ( name && email){
+        alert('Form submitted', {name, email});
+
+        // validation
+        if ( name && email){
+            alert(`Thank you, ${name}`);
+            form.reset();
+        }
+    }
+});
