e52c8071072f9d2df70a954e7e1664fad837d7b7
131MohitYadav <yadavjimohit54@gmail.com>
2025-11-18 17:00:35 +0530

    18 nov 25

--- JAVASCRIPT LANGUAGE/DomEx.js
+++ JAVASCRIPT LANGUAGE/DomEx.js
@@ -109,4 +109,9 @@
                             
 
 
-                            
+          // Dom Manipulation // 
+
+          document.getElementById('change-text').addEventListener('click', function(){
+            document.getElementById('main-heading').textContent = 'Text Changed!';
+            document.getElementById('main-heading').style.color = 'red';
+          })
