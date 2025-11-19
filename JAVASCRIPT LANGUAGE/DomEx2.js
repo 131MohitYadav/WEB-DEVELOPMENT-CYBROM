2fedc7f4a759a7fa91c5f4d2a334e8a2898c2c98
131MohitYadav <yadavjimohit54@gmail.com>
2025-11-19 18:52:07 +0530

    19 nov 25

--- JAVASCRIPT LANGUAGE/DomEx2.js
+++ JAVASCRIPT LANGUAGE/DomEx2.js
@@ -1,5 +1,12 @@
 // DOM Manipulation
 document.getElementById('change-text').addEventListener('click', function(){
-    document.getElementById('main-heading').textContent = 'Text Changed';
+    document.getElementById('main-heading').textContent = 'Hii Kajal!!';
     document.getElementById('main-heading').style.color = 'red';
+})
+
+document.getElementById('add-element').addEventListener('click', function() {
+    const newElement = document.createElement('div');
+    newElement.className = 'new-element';
+    newElement.textContent = 'New Element Added';
+    document.getElementById('content').appendChild(newElement);
 })
