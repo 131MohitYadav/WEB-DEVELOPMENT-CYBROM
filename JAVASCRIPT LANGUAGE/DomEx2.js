e3d6f6bfa29f7f3ba136a766528af882d497c343
131MohitYadav <yadavjimohit54@gmail.com>
2025-11-23 23:01:16 +0530

    23 nov 25

--- JAVASCRIPT LANGUAGE/DomEx2.js
+++ JAVASCRIPT LANGUAGE/DomEx2.js
@@ -8,6 +8,7 @@ document.getElementById('add-element').addEventListener('click', function() {
     const newElement = document.createElement('div');
     newElement.className = 'new-element';
     newElement.textContent = 'Welcome to MOhit in Dom Manipulation';
+    newElement.textContent = 'Hello Mohti Yadav';
     document.getElementById('content').appendChild(newElement);
 })
 
