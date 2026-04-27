3f6fb657e6eeb58062ff5b08d2876796a5cf936a
131MohitYadav <yadavjimohit54@gmail.com>
2026-04-27 22:08:00 +0530

    27 apr 26

--- React Js/Test/src/pages/Login.jsx
+++ React Js/Test/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Login = () => {
   const navigate = useNavigate();
